fe1ce150b1dba3d3a7b09041adbe5c98bcfecb29
mohamedkhameSss <113071196+mohamedkhameSss@users.noreply.github.com>
2023-06-29 02:48:40 +0300

    provider commit

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/next/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
   providers: [
